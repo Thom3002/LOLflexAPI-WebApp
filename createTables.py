0382cf8092d2b9192b6fb629dd54102e184ee1d7
Thomas de Mello Oliva <thomas.mello301@gmail.com>
2023-12-13 21:07:59 -0300

    adiciona cs/min + ajustes + comentarios

--- createTables.py
+++ createTables.py
@@ -2,9 +2,12 @@ from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 from datetime import datetime
 
-API_KEY = 'RGAPI-9bcfbf80-3881-4a06-81e0-b12703c43d3f'
-SUMMONER_NAME = 'mikasa 7'
-SEASON_ATUAL = True
+# macros
+API_KEY = 'RGAPI-9bcfbf80-3881-4a06-81e0-b12703c43d3f' # API key da Riot Games
+SUMMONER_NAME = 'mikasa 7' # Nome do jogador cujo histórico de partidas será analisado
+SEASON_ATUAL = True # Variável que indica se a partida é da season atual (True) ou não (False)
+
+# Lista de nomes que serão analisados caso estejam na partida do jogador (SUMMONER_NAME)
 interested_names = ["lucjam", "xLegenderx", "Living Paradox", "mikasa 7", "NeoLight", "Kuroda el Bruto", "Tom Platz", "dudinha", "EU COMO DUDINHA", "Kolaltia", "mikasa ツ", "tsidkenu963", "Taylor Swiftness"]
 
 watcher = LolWatcher(API_KEY)
@@ -19,32 +22,37 @@ except ApiError as err:
     else:
         raise
 
+# Pega o histórico de partidas do jogador
 my_matches = watcher.match.matchlist_by_puuid(my_region, me['puuid'])
-df = pd.DataFrame(columns=["MatchNumber", "SummonerName", "Role", "Champion", "Kills", "Deaths", "Assists","KDA", "TotalMinionsKilled", "Date"])
-df_provisorio = pd.DataFrame(columns=["MatchNumber", "SummonerName", "Role", "Champion", "Kills", "Deaths", "Assists","KDA", "TotalMinionsKilled", "Date"])
+
+# Cria os dataFrames que serão utilizados
+df = pd.DataFrame(columns=["MatchNumber", "SummonerName", "Role", "Champion", "Kills", "Deaths", "Assists","KDA", "CS","CS/min", "Date"])
+df_provisorio = pd.DataFrame(columns=["MatchNumber", "SummonerName", "Role", "Champion", "Kills", "Deaths", "Assists","KDA", "CS","CS/min", "Date"])
 match_index = 0
 num_participantes = 0
 aram_count = 0
+
+# Verifica cada partida do histórico do jogador (últimas 20 partidas)
 for match in my_matches:
     print("Analysing match " + str(match_index) + "\n******************\n")
     match_detail = watcher.match.by_id(my_region, match)
     timestamp_ms = match_detail['info']['gameEndTimestamp']
     timestamp_s = timestamp_ms / 1000.0  # Convert from ms to s
     date = datetime.fromtimestamp(timestamp_s)  
-    # print("DATA " + str(date))
-    # Create a datetime object for 08/01/2023
+    print("DATA " + str(date))
+    
+    # Verifica se o jogo é da season atual (depois de 08/01/2023)
     compare_date = datetime.strptime('08/01/2023', '%m/%d/%Y')
 
-    # Check if date is after 08/01/2023
     if date > compare_date:
-        # print("Date is after 08/01/2023")
-        # print(date)
         SEASON_ATUAL = True
     else:
         print("Date is not after 08/01/2023")
-        # print(date)
         SEASON_ATUAL = False
+        
     # print(match_detail)
+    
+    # Verifica se a partida é uma partida clássica
     if(match_detail['info']['gameMode'] != 'CLASSIC'):
         print("Match " + str(match_index) + " is not a classic match. Skipping...")
         if match_detail['info']['gameMode'] == 'ARAM':
@@ -54,12 +62,16 @@ for match in my_matches:
         continue
     for participant in match_detail['info']['participants']:
         summoner_name = participant['summonerName']
-        print("****particpants summoner name: " + participant['summonerName'])
+        # print("****participants summoner name: " + participant['summonerName'])
         if summoner_name in interested_names:
             print("Summoner " + summoner_name + " is in the list of interested names. Adding to the table...")
             role = participant['individualPosition']
+            total_minions_killed = participant['totalMinionsKilled'] + participant['neutralMinionsKilled']
+            cs_per_min = total_minions_killed / (match_detail['info']['gameDuration'] / 60)
             if (role == 'UTILITY'):
                 role = 'SUPPORT'
+            elif (role == 'BOTTOM'):
+                role = 'ADC'
             participant_row = {
                 'MatchNumber': "match " + str(match_index),
                 'SummonerName': summoner_name,
@@ -69,7 +81,8 @@ for match in my_matches:
                 'Deaths': participant['deaths'],
                 'Assists': participant['assists'],
                 'KDA': (participant['kills'] + participant['assists']) / participant['deaths'] if participant['deaths'] != 0 else participant['kills'] + participant['assists'],
-                'TotalMinionsKilled': participant['totalMinionsKilled'],
+                'CS': total_minions_killed,
+                'CS/min' : cs_per_min,
                 'Date': date
             }
             num_participantes += 1
@@ -77,34 +90,34 @@ for match in my_matches:
                 print("Invalid role for " + participant_row['SummonerName'] + " in match " + str(match_index))
             df_new_row = pd.DataFrame(participant_row, index=[match_index])
             df_provisorio = pd.concat([df_provisorio, df_new_row])
-            
-    if num_participantes >= 3 and SEASON_ATUAL: # Verifica se possui 3 ou mais participantes
+    
+    # Somente adiciona na tabela se a partida possuir 3 ou mais participantes da lista de nomes interessados
+    if num_participantes >= 3 and SEASON_ATUAL: 
         print("A partida " + str(match_index) + " tem " + str(num_participantes) + " participantes. Adicionando a tabela final...")
         df = pd.concat([df, df_provisorio])
     num_participantes = 0
     match_index += 1
 
 
-# Limita a quantidade de casas decimais para 2
-# pd.set_option('display.float_format', '{:.2f}'.format) 
-
 # Remove linhas duplicadas
 df = df.drop_duplicates()
 
 # Agrupa a tabela de partidas por role para cada jogador, somando os valores
-sum_df = df.groupby(['SummonerName', 'Role'])[['Kills', 'Deaths', 'Assists', 'KDA', 'TotalMinionsKilled']].sum().reset_index()
+sum_df = df.groupby(['SummonerName', 'Role'])[['Kills', 'Deaths', 'Assists', 'KDA', 'CS', 'CS/min']].sum().reset_index()
 # print(sum_df)
 
-# Count the number of matches per role for each player
+# Conta o número de partidas por role para cada jogador
 matches_per_role = df.groupby(['SummonerName', 'Role']).size().reset_index(name='Matches')
 
 # Calcula a média de cada jogador por role
-average_df = df.groupby(['SummonerName', 'Role'])[['Kills', 'Deaths', 'Assists','KDA', 'TotalMinionsKilled']].mean().astype(float).round(2).reset_index()
+average_df = df.groupby(['SummonerName', 'Role'])[['Kills', 'Deaths', 'Assists','KDA', 'CS', 'CS/min']].mean().astype(float).round(1).reset_index()
 print(average_df)
 
+# Adiciona a coluna de número de partidas na tabela de médias
 average_df = pd.merge(average_df, matches_per_role, on=['SummonerName', 'Role'])
 # print(average_df)
 
+# Converte as tabelas para csv
 sum_df.to_csv('tabelas/grouped.csv', index=False)
 average_df.to_csv('tabelas/average.csv', index=False)
 df.to_csv('tabelas/matches.csv', index=False)
